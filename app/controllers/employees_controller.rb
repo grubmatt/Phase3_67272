6de3bcc86e73e37e076455226a8aded4fcb0b901
Matt <mattgruber61@gmail.com>
2016-03-15 01:00:34 -0400

    Added links for employee name and store in assignment view

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -10,6 +10,7 @@ class EmployeesController < ApplicationController
   # GET /employees/1
   # GET /employees/1.json
   def show
+    @assignments = @employee.assignments
   end
 
   # GET /employees/new
